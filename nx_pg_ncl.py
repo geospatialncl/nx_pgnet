e3e9c5e98e5563956d93b2cf62e450384fbeb151
Tom Holderness <tom.holderness@ncl.ac.uk>
2012-01-23 16:07:36 +0000

    Added comments for development. Reduced to 80 chars per line.

--- nx_pg_ncl.py
+++ nx_pg_ncl.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env  python
 # -*- coding: utf-8 -*-
 """
-nx_pg - Generates a networkx.DiGraph from PostGIS table.
+nx_pg_ncl - Procedural module for NetworkX read/write of PostGIS tables.
+
+Development notes.
+
+1) Based on nx_shp.py from NetworkX source.
+    - Should be converted to OO/Class based module in the future for better 
+        functionality
+2) Currently (v 0.1) contains minimal error checking! 
+
+3) When using read_pg() only include a nodes table if this corresponds to 
+network nodes (i.e. edge junctions) otherwise output nodes table will break!
+
+4) Outputs do not currently specify CRS/EPSG. Use a friendly GIS (QGIS) to set
+on load.
+
+5) Have tried to adhere to Python PEP8 style as much as possible.
+
+6) Included main function is just for development and will be removed.
 
 """
 __author__ = "Tom Holderness"
@@ -12,9 +29,10 @@ __version__ = "0.1"
 import networkx as nx
 import osgeo.ogr as ogr
 import pggkgetpass as gp
-#import nx_shp
+
+# Get database password from gnomekeyring (for development purposes only).
+# REF: http://www.students.ncl.ac.uk/tom.holderness/pythongnomekeyring.php
 PGS = gp.getpass('login', 'pg-ceg-tyndall')
-#PGS = 'postgres'
 
 def getfieldinfo(lyr, feature, flds):
     ''' Get information about fields - borrowed from nx_shp.py'''
@@ -22,7 +40,7 @@ def getfieldinfo(lyr, feature, flds):
     return [f.GetField(f.GetFieldIndex(x)) for x in flds]
     
 def read_pg(conn, table_edges, table_nodes=None, directed=True):
-    '''Function to convert geometry from PostGIS table to Network.'''
+    '''Read PostGIS table and return NetworkX graph.'''
     # Create Directed graph to store output
     if directed is True:
         net = nx.Graph()
@@ -37,73 +55,89 @@ def read_pg(conn, table_edges, table_nodes=None, directed=True):
             for findex in xrange(lyr.GetFeatureCount()):
                 # Get a specific feature
                 f = lyr.GetFeature(findex+1)
-                
                 if f is None:
-                    pass
+                    pass # Hack to catch any returned features which are None. 
                 else:
                     flddata = getfieldinfo(lyr, f, flds )
                     attributes = dict(zip(flds, flddata))
                     attributes["TableName"] = lyr.GetName()
                     # Get the geometry for that feature
                     geom = f.GetGeometryRef()
-                    # We've got a Multiline geometry so split into line segments
+                    
+                    # Multiline geometry so split into line segments
                     if ogr.Geometry.GetGeometryName(geom) == 'MULTILINESTRING':
-                    #print f.GetGeomertyTypeName()
                         for line in geom:
-                            #print "Exporting multiline geometry"
                             # Get points in line
                             n = line.GetPointCount()
                             # Get the attributes (akin to nx_shp)
                             attributes["Wkb"] = ogr.Geometry.ExportToWkb(line)
                             attributes["Wkt"] = ogr.Geometry.ExportToWkb(line)
                             attributes["Json"] = ogr.Geometry.ExportToWkb(line)
-                            #print type(line.GetPoint(0))
-                            net.add_edge(line.GetPoint_2D(0), line.GetPoint_2D(n-1), attributes)
+                            net.add_edge(line.GetPoint_2D(0), 
+                                         line.GetPoint_2D(n-1), attributes)
+                    # Line geometry
                     elif ogr.Geometry.GetGeometryName(geom) == 'LINESTRING':
-                        #print "Exporting line geometry"
                         n = geom.GetPointCount()
                         attributes["Wkb"] = ogr.Geometry.ExportToWkb(geom)
                         attributes["Wkt"] = ogr.Geometry.ExportToWkb(geom)
                         attributes["Json"] = ogr.Geometry.ExportToWkb(geom) 
-                        ###print geom.GetPoint_2D(0), attributes['FID'] 
-                        net.add_edge(geom.GetPoint_2D(0), geom.GetPoint_2D(n-1), attributes)
+                        net.add_edge(geom.GetPoint_2D(0), 
+                                         geom.GetPoint_2D(n-1), attributes)
+                    # Point geometry                    
                     elif ogr.Geometry.GetGeometryName(geom) == 'POINT':
-                        #print "Exporting point geometry"
                         net.add_node((geom.GetPoint_2D(0)), attributes)
                     else:
                         raise ValueError, "PostGIS geometry type not supported."
     return net
 
 def netgeometry(key, data):
-        if data.has_key('Wkb'):
-            geom = ogr.CreateGeometryFromWkb(data['Wkb'])
-        elif data.has_key('Wkt'):
-            geom = ogr.CreateGeometryFromWkt(data['Wkt'])
-        elif type(key[0]) == 'tuple': # edge keys are packed tuples
-            geom = ogr.Geometry(ogr.wkbLineString)
-            _from, _to = key[0], key[1]
-            geom.SetPoint(0, *_from)
-            geom.SetPoint(1, *_to)
-        else:
-            geom = ogr.Geometry(ogr.wkbPoint)
-            geom.SetPoint(0, *key)
-        return geom
+    '''Create OGR geometry from NetworkX Graph Wkb/Wkt attributes.
+
+    Borrowed from nx_shp.py.    
+    '''
+    if data.has_key('Wkb'):
+        geom = ogr.CreateGeometryFromWkb(data['Wkb'])
+    elif data.has_key('Wkt'):
+        geom = ogr.CreateGeometryFromWkt(data['Wkt'])
+    elif type(key[0]) == 'tuple': # edge keys are packed tuples
+        geom = ogr.Geometry(ogr.wkbLineString)
+        _from, _to = key[0], key[1]
+        geom.SetPoint(0, *_from)
+        geom.SetPoint(1, *_to)
+    else:
+        geom = ogr.Geometry(ogr.wkbPoint)
+        geom.SetPoint(0, *key)
+    return geom
 
 def create_feature(geometry, lyr, attributes=None):
-  feature = ogr.Feature(lyr.GetLayerDefn())
-  feature.SetGeometry(geometry)
-  if attributes != None:
-      for field, data in attributes.iteritems(): 
-         feature.SetField(field,data)
-  lyr.CreateFeature(feature)
-  feature.Destroy()
+    '''Create an OGR feature in specified layer with geometry and attributes.'''
+    feature = ogr.Feature(lyr.GetLayerDefn())
+    feature.SetGeometry(geometry)
+    if attributes != None:
+        for field, data in attributes.iteritems(): 
+            feature.SetField(field,data)
+    lyr.CreateFeature(feature)
+    feature.Destroy()
+
+def getlayer(conn, layername):
+    '''Get a PostGIS table (layer) by name and return as OGR layer,
+        else return None.
+    
+    "Fixes (i.e. hacks)" OGR inconsistent error reporting'''
+    # Disable Error if table is none.
+    ogr.DontUseExceptions()
+    layer = conn.GetLayerByName(layername)
+    ogr.UseExceptions()
+    return layer
   
 def update_graph_table(conn, graph, graph_name, edge_table, node_table):
-    
-    # Turn off buggy OGR exceptions for this call (shoudl just return Null)
+    '''Update graph table or create if doesn't exist with agreed schema.'''
+    '''
     ogr.DontUseExceptions()
     tblgraphs = conn.GetLayerByName('graphs')
     ogr.UseExceptions()
+    '''
+    tblgraphs = getlayer(conn, 'graphs')
     '''
     for layer in conn:
         print layer.GetName()
@@ -113,8 +147,11 @@ def update_graph_table(conn, graph, graph_name, edge_table, node_table):
         else:
             tblgraphs = None
     '''
-    
+    # tblgraphs doesn't exist so create with new features.
+    # This is hard coded - should be reworked to be dynamic, similar style to..
+        #...write_pg attributes)
     if tblgraphs is None:
+        # tblgraphs is ogr.wkbNone (i.e. non-spatial)
         tblgraphs = conn.CreateLayer('graphs', None, ogr.wkbNone)
         Field_GraphName = ogr.FieldDefn('GraphName', ogr.OFTString)
         tblgraphs.CreateField(Field_GraphName)
@@ -130,28 +167,36 @@ def update_graph_table(conn, graph, graph_name, edge_table, node_table):
     
         Field_MultiGraph = ogr.FieldDefn('MultiGraph', ogr.OFTString)
         tblgraphs.CreateField(Field_MultiGraph)
-    print 'read feature tblgraphs...'
+    # Now add the data.    
     feature = ogr.Feature(tblgraphs.GetLayerDefn())
-    
     feature.SetField('GraphName', graph_name)
     feature.SetField('Nodes', edge_table)
     feature.SetField('Edges', node_table)
+    print nx.is_directed(graph)
     if nx.is_directed(graph):
         feature.SetField('Directed', 1)
     else:
         feature.SetField('Directed', 0)
-    # Still needs to be implemented.
+    # Multigraph still needs to be implemented.
     feature.SetField('MultiGraph', 0)
-    print "tblgraphs.CreateFeature()"
+    # Create feature, clear pointer.
     tblgraphs.CreateFeature(feature)
     feature.Destroy()
 
 def write_pg(conn, network, tablename_prefix, overwrite=False):
-    '''Function to write Network with geometry to PostGIS edges and nodes tables.'''
-    G = network
+    '''Write NetworkX (with geom) to PostGIS edges and nodes tables.
+    
+    Will also update graph table.    
+    '''
+
+    G = network # Use G as network, convention from earlier code.
     tbledges = tablename_prefix+'_edges'
     tblnodes = tablename_prefix+'_nodes'
-        
+    
+    edges = getlayer(conn, tbledges)
+    nodes = getlayer(conn, tblnodes)    
+
+    '''
     for layer in conn:    
         if layer.GetName() == tbledges:
             edges = layer
@@ -165,6 +210,7 @@ def write_pg(conn, network, tablename_prefix, overwrite=False):
             break
         else:
             nodes = None
+    '''
     
     if edges is None:
         edges = conn.CreateLayer(tbledges, None, ogr.wkbLineString)
@@ -180,10 +226,8 @@ def write_pg(conn, network, tablename_prefix, overwrite=False):
             conn.DeleteLayer(tblnodes)
             nodes = conn.CreateLayer(tblnodes, None, ogr.wkbPoint)
 
-    ## For all the nodes add an index (based on FID?) attributes.
-      # then pull this attributes from edge information to have start and end node.
-      # for e i n G
-      # limit on the number of nodes as run out of integers?
+    # For all the nodes add an index.
+    # Warning! This will limit unique node index to sys.maxint 
     nid = 0
     fields = {}
     attributes = {}
@@ -191,9 +235,6 @@ def write_pg(conn, network, tablename_prefix, overwrite=False):
     for n in G.nodes(data=True):
         G.node[n[0]]['NodeID'] = nid
         data = G.node[n[0]]
-        #print G.node[n[0]].values()
-        #data = G.node[n[0]].values() or [{}]
-        #print data
         g = netgeometry(n[0], data)
         # Loop through data in nodes
         for key, data in n[1].iteritems():
@@ -214,18 +255,17 @@ def write_pg(conn, network, tablename_prefix, overwrite=False):
                      attributes[key] = data
         create_feature(g, nodes, attributes)
         nid += 1
-
-    #for e in G.edges(data=True):
-         #print e
+    # Repeat similar operation for Edges (should put in function at some point!)
     fields = {}
     attributes = {}
-    eid = 0
+    eid = 0 # edge_id
     for e in G.edges(data=True):
         data = G.get_edge_data(*e)
         #print 'edge data', data
         g = netgeometry(e, data)
-        e0 = e[0]
-        e1 = e[1]
+        e0 = e[0]   # edge start node
+        e1 = e[1]   # edge end node
+        # Add attributes as defined in schema
         G[e0][e1]['EdgeID'] = eid
         G[e0][e1]['Node_F'] = G.node[e[0]]['NodeID']
         G[e0][e1]['Node_T'] = G.node[e[1]]['NodeID']
@@ -250,21 +290,30 @@ def write_pg(conn, network, tablename_prefix, overwrite=False):
          # Create the feature with attributes
         create_feature(g, edges, attributes)
     update_graph_table(conn, G, tablename_prefix, tbledges, tblnodes)
-
+    # Done, clear pointers
     nodes, edges = None, None    
 
 def main():
-    '''Main function - testing OGR Python PostGIS API for passing data to NetworkX.'''
-    # Create Connecton
+    '''Testing nx_pg_ncl.py for NetworkX read/write of PostGIS tables.'''
     
-    conn = ogr.Open("PG: host='ceg-tyndall' dbname='tyndall_data' user='postgres' password="+PGS+"")  
+    # Create a source connecton
+    conn = ogr.Open("PG: host='ceg-tyndall' dbname='tyndall_data' \
+        user='postgres' password="+PGS+"")  
     
     print 'reading network'
+    # Build network in Python (see module notes about input nodes tables.)
     net = read_pg(conn, 'LightRail_Baseline') #, 'LightRail_Baseline_Stations')
+    
+    # Do some stuff with network.
+    
+    # Write network to PostGIS tables (use the same connection in this case).
     print 'writing network'
-    #conn = ogr.Open("PG: host='127.0.0.1' dbname='MIDAS-SPATIAL' user='postgres' password="+PGS+"")  
+    #conn = ogr.Open("PG: host='127.0.0.1' dbname='MIDAS-SPATIAL' \
+    #   user='postgres' password="+PGS+"")  
+    
     write_pg(conn, net, 'lightrail_baseline_nd', overwrite=True)    
-    print "done."
+    
+    print "Done"
     conn = None
     
 if __name__ == "__main__":
