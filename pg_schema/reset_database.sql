13c6c52f4ba24939c679df33199628a5dd3ce7bb
Tom Holderness <tom.holderness@ncl.ac.uk>
2012-03-05 16:42:20 +0000

    Added folder "pg_schema/" containing latest version of schema files.

---
+++ pg_schema/reset_database.sql
@@ -0,0 +1,6 @@
+﻿-----------------------RESET THE DATABASE--------------------------
+SELECT * FROM ni_reset_database('public');
+
+--delete old network
+SELECT * FROM ni_delete_network('test1');
+SELECT * FROM ni_delete_network('test2');
