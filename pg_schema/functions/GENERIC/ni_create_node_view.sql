8916707668d3090af7d5a5243335c16e42968ee3
David Alderson <David.Alderson@ncl.ac.uk>
2013-01-04 17:11:35 +0000

    change of ST_Dimension to ST_NDims

--- pg_schema/functions/GENERIC/ni_create_node_view.sql
+++ pg_schema/functions/GENERIC/ni_create_node_view.sql
@@ -1,30 +1,21 @@
 
-CREATE OR REPLACE FUNCTION ni_create_node_view(character varying, character varying, character varying, integer, integer)
+CREATE OR REPLACE FUNCTION ni_create_node_view(character varying)
   RETURNS character varying AS
 $BODY$
 DECLARE
-
     --user supplied table prefix
     table_prefix ALIAS for $1;
-	
-	--schema name
-	schema_name ALIAS for $2;
     
-	--geometry column name for Node table
-    geometry_column_name ALIAS for $3;
-	
-    --to store SRID of geometry in Node table
-    SRID ALIAS for $4;
-	
-	--to store coordinate dimension of coordinates in geometry of Node table
-    coordinate_dimension ALIAS for $5;
-	
     --constant node table suffix
     node_table_suffix varchar := '_Nodes';
     node_view_suffix varchar := '_View_Nodes';
     new_node_view_name varchar := '';
     node_table_name varchar := '';
-	    
+	
+    --to hold the information on the geometry columns of the node table
+    SRID integer := 0;
+    dims integer := 0;
+    
     --geometry_column_record_count
     geometry_column_record_count integer := 0;
     
@@ -34,6 +25,9 @@ DECLARE
     --constant geometry column table name
     geometry_column_table_name varchar := 'geometry_columns';
     
+    schema_name varchar := 'public';
+    
+    geometry_column_name varchar := 'geom';
 BEGIN
     
     --original node table name
@@ -51,14 +45,15 @@ BEGIN
     --retrieve the SRID of the node view
     EXECUTE 'SELECT ST_SRID(geom) FROM '||quote_ident(node_table_name) INTO SRID;
     
-    --retrieve the coordinate_dimension of the node view
-    EXECUTE 'SELECT ST_Dimension(geom) FROM '||quote_ident(node_table_name) INTO coordinate_dimension;
+    --retrieve the dims of the node view
+    --EXECUTE 'SELECT ST_Dimension(geom) FROM '||quote_ident(node_table_name) INTO dims;
+	EXECUTE 'SELECT ST_NDims(geom) FROM '||quote_ident(node_table_name) INTO dims;
     
     --retrieve the geometry type for the node view
     EXECUTE 'SELECT GeometryType(geom) FROM '||quote_ident(node_table_name) INTO node_geometry_type;
     
 	--add the node view to the geometry columns table
-	EXECUTE 'SELECT * FROM ni_add_to_geometry_columns('||quote_literal(new_node_view_name)||', '''', '||quote_literal(schema_name)||', '||quote_literal(geometry_column_name)||', '||coordinate_dimension||','||SRID||','||quote_literal(node_geometry_type)||')';
+	EXECUTE 'SELECT * FROM ni_add_to_geometry_columns('||quote_literal(new_node_view_name)||', '''', '||quote_literal(schema_name)||', '||quote_literal(geometry_column_name)||', '||dims||','||SRID||','||quote_literal(node_geometry_type)||')';
 	--return the new view name to the user
     RETURN new_node_view_name;
     
@@ -66,4 +61,4 @@ END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
-ALTER FUNCTION ni_create_node_view(character varying, character varying, character varying, integer, integer) OWNER TO postgres;
+ALTER FUNCTION ni_create_node_view(character varying) OWNER TO postgres;
